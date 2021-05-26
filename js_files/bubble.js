33fe24b4f9e057c1d9309ad017a9934bc34c0e3e
AshishRajIITI <ashishiiti19@gmail.com>
2021-05-26 21:51:19 +0530

    button stylying

--- js_files/bubble.js
+++ js_files/bubble.js
@@ -11,7 +11,7 @@ function mySwap(el1, el2) {
 
 async function bubbleSort(delay=mainDelay) {  
   
-
+  disable();
   var i,j;
   for ( i = 0; i < n; i++) {
     for ( j = 0; j < n-1- i; j++) {
---
+++ js_files/disable.js
@@ -0,0 +1,8 @@
+function disable(){
+    document.getElementById("newArray").disabled=true;
+    document.getElementById("bubbleSort").disabled=true;
+    document.getElementById("selectionSort").disabled=true;
+    document.getElementById("insertionSort").disabled=true;
+    document.getElementById("mergeSort").disabled=true;
+    document.getElementById("quickSort").disabled=true;
+}
